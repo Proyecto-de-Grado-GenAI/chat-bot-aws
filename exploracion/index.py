20796893166f81c791488985a037ad519cc162d4
Brian Rivera <69652100+BraitoRX@users.noreply.github.com>
2024-05-07 02:19:02 -0500

    chore: Update exploracion/index.py to include Llama3 prompt definition and bedrockQuestion function

--- exploracion/index.py
+++ exploracion/index.py
@@ -1,6 +1,13 @@
 import boto3, json
-from chatResponder import ChatResponder
 from botocore.config import Config
+from chatResponder import ChatResponder
+import os
+import logging
+import re
+
+
+logger = logging.getLogger()
+logger.setLevel("INFO")
 
 bedrock = boto3.client("bedrock-runtime", config=Config(region_name="us-east-1"))
 
@@ -9,8 +16,16 @@ bedrock_agent_runtime = boto3.client(
     region_name="us-east-1",
 )
 
+# Query the knowledge base to retrieve the context for the question RAG
 
-def retrieve(query, kbId, numberOfResults=1):
+def extract_messages_from_chat(chat_string):
+    pattern = r"(Human|Assistant): (.*?)\n(?=Human|Assistant|$)"
+    matches = re.findall(pattern, chat_string, re.DOTALL)
+    messages = [msg.strip() for _, msg in matches]
+    messages.pop()
+    return messages
+
+def retrieveFromKnowledgeBase(query, kbId, numberOfResults=1):
     return bedrock_agent_runtime.retrieve(
         retrievalQuery={"text": query},
         knowledgeBaseId=kbId,
@@ -19,9 +34,7 @@ def retrieve(query, kbId, numberOfResults=1):
         },
     )
 
-
-def bedrockS(prompt, question, modelId="anthropic.claude-v2"):
-    response_knowledge_base = retrieve(question, "FFUYGR42Y1")["retrievalResults"]
+def insertContext(question, response_knowledge_base):
     context = ""
     contador = 0
     for i in response_knowledge_base:
@@ -30,42 +43,70 @@ def bedrockS(prompt, question, modelId="anthropic.claude-v2"):
         context += f"Source:{str(contador)} " + i["location"]["s3Location"]["uri"] + "\n"
         contador += 1
 
-    question = f"""{question}\n\n{context}"""
+    question_with_context = f"""{question}\n\n{context}"""
+    return question_with_context
 
-    print(prompt)
+# PROMPT DEFINITION by LLM
 
+def define_Llama3_prompt(history,system_prompt, question_with_context):
+    SystemPrompt = f"""<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n{system_prompt}<|eot_id|>"""
+    waitForAssistantPrompt = f"""<|start_header_id|>assistant<|end_header_id|>"""
+    init = SystemPrompt
+    for i in range(len(history)):
+        if i % 2 == 0:
+            init += f"""<|start_header_id|>user<|end_header_id|>\n{history[i]}<|eot_id|>"""
+        else:
+            init += f"""<|start_header_id|>assistant<|end_header_id|>\n{history[i]}<|eot_id|>"""
+    init += f"""<|start_header_id|>user<|end_header_id|>\n{question_with_context}<|eot_id|>"""
+    init += waitForAssistantPrompt
+    return init
+
+
+
+supported_models = {"meta.llama3-8b-instruct-v1:0":define_Llama3_prompt, "meta.llama3-70b-instruct-v1:0":define_Llama3_prompt}
+
+#TODO ADD knowledge base ID to the call , temperature and top_p and max_gen_len to the call and system prompt and number of results knowledge base y si se usa o no la Knowledge base
+
+def bedrockQuestion(history, question, modelId):
     system_prompt = """Eres un agente de inteligencia artificial muy especializado en la arquitectura de software,
     das respuestas en el mismo lenguaje que te preguntan y además tienes acceso a una base de conocimiento 
     con información relevante para responder preguntas relacionadas al ADD 3.0 y que se manifiesta como Context bajo la pregunta que se te realiza,
     además siempre proporcionas en la parte final de tus respuestas la fuente de la información exacta que utilizaste para responder la pregunta.
     En caso de que la información que te proporciono no sea suficiente para responder la pregunta, por favor házmelo saber para poder proporcionarte más información o contexto.
     Recuerda que siempre debes responder en el mismo lenguaje que te preguntan y que debes explicar de una forma detallada y entendible para un arquitecto de software.
+    No respondes nada que no sea relacionado a la arquitectura de software y al ADD 3.0.
     """
-    prompt = f"""<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n{ system_prompt }\n<|eot_id|><|start_header_id|>user<|end_header_id|>\n{ question }\n<|eot_id|><|start_header_id|>assistant<|end_header_id|>"""
+    
+    if modelId in supported_models:
+        response_knowledge_base_query = retrieveFromKnowledgeBase(question, "FFUYGR42Y1")["retrievalResults"]
+        question_with_context = insertContext(question, response_knowledge_base_query)
+        historial = extract_messages_from_chat(history)
+        print(historial)
+        prompt = supported_models[modelId](historial, system_prompt, question_with_context)
+        response = bedrock.invoke_model(
+            body=json.dumps(
+                {
+                    "prompt": prompt,
+                    "temperature": 0.5,
+                    "top_p": 0.999,
+                    "max_gen_len": 2048,
+                }
+            ),
+            modelId=modelId,
+        )
 
-    response = bedrock.invoke_model(
-        body=json.dumps(
-            {
-                "prompt": prompt,
-                "temperature": 0.5,
-                "top_p": 0.999,
-                "max_gen_len": 2048,
-            }
-        ),
-        modelId=modelId,
-    )
+        raw_body = response["body"].read().decode("utf-8")
+        response_json = json.loads(raw_body)
 
-    raw_body = response["body"].read().decode("utf-8")
-    response_json = json.loads(raw_body)
-
-    return [*response_json.values()][0]
+        return [*response_json.values()][0]
+    else:
+        return "Model not supported"
 
 
 def handler(event, context):
-
     chatResponder = ChatResponder(event["conversationData"]["id"])
     try:
-        response = bedrockS(
+        response = bedrockQuestion(
             event["chatString"],
             event["userInput"]["message"],
             event["userInput"]["model"]["model"],
@@ -79,3 +120,4 @@ def handler(event, context):
 
     # Mark metadata as done responding
     chatResponder.publish_agent_stop_responding()
+
